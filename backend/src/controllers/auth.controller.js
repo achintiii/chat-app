3411cfa95ce02801521ff4cf4197b9576b51e13d
achintiii <achintiii@outlook.com>
2025-01-02 14:23:50 -0800

    finished all authentication backend functionality for jwt. Now going to work on message backend functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -91,5 +92,30 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePicture} = req.body;
+        const userId = req.user._id;
+        if (!profilePicture) {
+            return res.status(400).json({ message: "Profile Pic is Required" });
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePicture)
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePicture:uploadResponse.secure_url}, { new: true });
+    }
+    catch(error){
+        console.error("Error in user updateProfile:", error.message);
+        res.status(500).json({ message: "Server error" });
+    }
 
 
+}
+export const checkAuth = (req, res) => {
+    try {
+        res.status (200).json(req.user);
+
+    }
+    catch (error) {
+        console.error("Error in user checkAuth:", error.message);
+        res.status(500).json({ message: "Server error" });
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import {login, logout, signup} from '../controllers/auth.controller.js';
 import {protectRoute} from '../middleware/auth.middleware.js';
+import {updateProfile} from '../controllers/auth.controller.js';
+import {checkAuth} from '../controllers/auth.controller.js';
 const router = express.Router();
 
 // Signup route
-router.get('/signup', signup);
+router.post('/signup', signup);
 
 // Login route
-router.get('/login', login);
+router.post('/login', login);
 
 // Logout route
-router.get('/logout', logout);
+router.post('/logout', logout);
 
-//router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth)
 
 export default router;
