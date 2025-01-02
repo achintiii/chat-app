f022a58187156e632dee23c02dc8414d4ef25d14
achintiii <achintiii@outlook.com>
2025-01-02 12:48:54 -0800

    finished jwt token for signup as well as signup functionality in authcontroller. Got mongoDB up and running

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,36 @@
-export const signup = (req, res) => {
-    res.send('signup route');
+import User from "../models/user.model";
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+    const{fullName, email, password} = req.body;
+    try {
+        //hash password
+        if (password.length < 6) {
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
+        }
+        const user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({message: "User already exists"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const User = new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+        if (newUser) {
+            // generate JWT token
+            
+        }
+        else {
+            return res.status(400).json({message: "User creation failed"});
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 export const login = (req, res) => {
     res.send('login route');
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,27 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
 
 const app = express();
 app.use("/api/auth", authRoutes);
 
 // Middleware
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/mernstack', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // API Routes
-app.get('/', (req, res) => {
-    res.send('Hello, MERN stack backend!');
+app.get("/", (req, res) => {
+  res.send("Hello, MERN stack backend!");
 });
 
-const PORT = 5001;
-app.listen(PORT, () => console.log(`Backend server running on port ${PORT}`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Backend server running on port ${PORT}`);
+  connectDB();
+});
