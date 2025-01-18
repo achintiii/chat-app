413ffcb4d6ec1b7eb56756b464d71392d05d767c
achintiii <achintiii@outlook.com>
2025-01-18 16:04:28 -0500

    trying to do deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,13 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import {app, server} from './lib/socket.js';
 
+import path from "path";
+
 
 dotenv.config();
 
+
+const __dirname = path.resolve();
 
 
 // Middleware
@@ -28,6 +32,14 @@ app.get("/", (req, res) => {
 // Connect to MongoDB and Start Server
 const PORT = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile*(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+  
 server.listen(PORT, () => {
   console.log("Socket server running on port 5001");
   connectDB();
